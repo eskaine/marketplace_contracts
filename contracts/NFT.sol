a3dbaa83155ee69e355bdf6e949821716f01cd92
eskaine <eskaineliu@gmail.com>
2022-05-31 22:43:16 +0800

    chore: setup alchemy and token contract

---
+++ contracts/NFT.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC721.sol";
+import "@openzeppelin/contracts/token/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+
+contract NFT is ERC721URIStorage {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    address contractAddress;
+
+    constructor(address marketplaceAddress) ERC721("Rave Tokens", "RVT") {
+        contractAddress = marketplaceAddress;
+    }
+
+    function createToken(string memory tokenURI) public returns (uint) {
+        _tokenIds.increment();
+        uint256 itemId = _tokenIds.current();
+
+        _mint(msg.sender, itemId);
+        _setTokenURI(itemId, tokenURI);
+        setApprovalForAll(contractAddress, true);
+
+        return itemId;
+    }
+}
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,16 +1,17 @@
-/**
-* @type import('hardhat/config').HardhatUserConfig
-*/
-// require('dotenv').config();
-// require("@nomiclabs/hardhat-ethers");
+require('dotenv').config();
 require("@nomiclabs/hardhat-waffle");
 
-// const { API_URL, PRIVATE_KEY } = process.env;
+const { API_KEY, SECRET_KEY } = process.env;
+
 module.exports = {
-   solidity: "0.8.1",
+   solidity: "0.8.0",
    networks: {
       hardhat: {
          chainId: 1337
-       },
+      },
+      goerli: {
+         url: `https://eth-goerli.alchemyapi.io/v2/${API_KEY}`,
+         accounts: [SECRET_KEY]
+      }
    },
 }
